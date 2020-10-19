70f1206e29684dbc676b2a3cfda2a44ca4f763bf
dom <domepiro87@yahoo.it>
2020-10-19 18:47:43 +0200

    base_struttura

--- molisana/routes/web.php
+++ molisana/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/prodotti', function () {
+    return view('prodotti');
+});
+
+Route::get('/news', function () {
+    return view('news');
 });
